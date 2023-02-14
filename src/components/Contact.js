08ebc93cc02eaa5424c3f6ea54936971b7269c08
Char Broadley <broadleychar@gmail.com>
2023-02-14 16:54:33 +0000

    added link to git hub and fixed linkedin link

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
 import linkedin from "../images/linkedin.png"
+import github from "../images/github.png"
 
 
 const Contact = () => {
@@ -7,8 +8,12 @@ const Contact = () => {
         <h3 className="contact-title">CONTACT INFO</h3>
             <p className="contact-info">
                 broadleychar@gmail.com</p>
-                    <a className="linkedin" href="http://www.linkedin.com/in/charlotte-broadley">
-                        <img src={linkedin} width="25" alt="linkedin">
+                    <a className="linkedin" href="http://www.linkedin.com/in/charbroadley">
+                        <img src={linkedin} width="25" alt="link to my linkedin">
+                        </img>
+                    </a>
+                    <a className="github" href="https://github.com/charbroadley">
+                        <img src={github} width="25" alt="link to my github">
                         </img>
                     </a>
         </section>
