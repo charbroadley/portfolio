ccd5c4ee5c399e934333623e05fe18c715718ecf
Char Broadley <broadleychar@gmail.com>
2023-02-14 16:40:39 +0000

    about me text updated, tech stack updated

--- src/components/Details.js
+++ src/components/Details.js
@@ -4,7 +4,8 @@ const Details = () => {
         <section className="details-section">
             <h3 className="header-about">ABOUT ME</h3>
                 <div className="about">
-                    <p className="about-text">I am an Edinburgh based software developer with a passion for problem-solving (absolutely love a challenge). I am always excited to learn and am eager to improve myself. My background is in design and administration which has enabled me to develop strong organisational and interpersonal skills. In my free time I love to be outdoors, and to run in the hills around Edinburgh.</p>
+                    <p className="about-text">I am an Edinburgh based junior software developer who has recently completed the CodeClan Professional Software Development course. After achieving a design degree I worked in administration before making the move into software development, however my time in these roles has enabled me to develop strong interpersonal and organisational skills. A passionate problem solver with natural creativity, I love the sense of achievement that comes from seeing my solutions come to life on the screen in front of me. I am excited to combine my previous skills with my new technical skills in a role that will enable me to embrace new challenges, further learning and self-development. In my free time I love to be outdoors, and to run in the hills around Edinburgh.
+                    </p>
                     <br/>
                 </div>
                 <h3 className="header-stack">STACK</h3>
@@ -15,17 +16,20 @@ const Details = () => {
                                 Flask<br/>
                                 Node.js<br/>
                                 Express.js<br/>
+                                Jest<br/>
+                                Cypress<br/>
                             </p>
                             <p>
-                                Javascript<br/>
+                                JavaScript<br/>
                                 React<br/>
                                 HTML5<br/>
                                 CSS3<br/>
                             </p>
-                            <p>
+                            <p> 
+                                Java<br/>
                                 PostgreSQL<br/>
                                 MongoDB<br/>
-                                Git
+                                Git<br/>
                             </p>
                         </p>
                 </div>
