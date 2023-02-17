bd4dcbd8df94fe5701bdb11dbd7739b7cf4e760a
Char Broadley <broadleychar@gmail.com>
2023-02-17 12:03:57 +0000

    working an exciting buttons

--- src/components/Project2.js
+++ src/components/Project2.js
@@ -11,7 +11,7 @@ const Project2 = () => {
                 <p>
                     A fun, interactive app using the Punk API. Users enter a number and are returned a suggested Brew Dog beer for their next pint, which they can mark as already drank, or add to their list to try next.
                     <br/><br/>
-                    <a href="https://github.com/charbroadley/punk_pints">View on GitHub</a>
+                    <a className="github-link" href="https://github.com/charbroadley/punk_pints">View on GitHub</a>
                 </p>
             </div>
             <img className="project-image" src={punkPintsScreengrab} alt="screengrab of Punk Pints app"></img>
