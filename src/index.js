007a19efcce018f4d89766a5c53cd05f38d3a990
Char Broadley <broadleychar@gmail.com>
2023-02-17 14:44:39 +0000

    media queries acceptable

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css'; // MEDIA QUERIES: DESKTOP
-import './tablet.css'; //MEDIA QUERIES: TABLET
 import './mobile.css'; // MEDIA QUERIES: MOBILE
 
 import App from './App';
