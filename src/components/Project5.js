9612aae8f8b47d78d4c1fc26a6fc073945beec1b
Char Broadley <broadleychar@gmail.com>
2023-05-16 17:11:20 +0100

    added button

--- src/components/Project5.js
+++ src/components/Project5.js
@@ -9,10 +9,13 @@ const Project5 = () => {
                 <h4>C#, Unity<br/>
                     Group, 2 weeks</h4>
                 <p>Our CodeClan capstone project. We made a 3D, first-person escape room style game, using the Unity game engine and C# scripts.<br/>
-                I was excited to work on a project which was totally different to anything I’d done before, and also to create a game which my friends and I would enjoy playing. 
+                I was excited to work on a project which was totally different to anything I’d done before, and also to create a game which my friends and I would enjoy playing.
                 <br/><br/>
                     <div className="button-container">
                         <a className="button" href="https://github.com/samuel-williams-ed/Escape_Game" target="_blank">View on GitHub</a>
+                    </div>
+                    <div className="button-container">
+                        <a className="button" href="https://play.unity.com/mg/other/children-of-the-bookcase" target="_blank">Play on Unity Play</a>
                     </div>
                 </p>
             </div>
